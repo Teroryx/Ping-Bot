d45951841b61104af31d5be5b7e1d765f562b749
Teroryx <enzoizinga23@gmail.com>
2025-11-10 19:43:17 +0100

    Delete ping.js

--- ping.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  const fetch = (await import('node-fetch')).default;
-  const url = "https://ping-bot.vercel.app/"; // ton lien Render ici
-  await fetch(url);
-  res.status(200).send("Ping envoyé !");
-}
