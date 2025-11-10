46cf4c4e14521e4445a2a2f8ce85c8be947fa1a5
Teroryx <enzoizinga23@gmail.com>
2025-11-10 19:41:11 +0100

    Fix URL in fetch call

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const fetch = (await import('node-fetch')).default;
-  const url = "https://https://ping-bot.vercel.app/"; // ton lien Render ici
+  const url = "https://ping-bot.vercel.app/"; // ton lien Render ici
   await fetch(url);
   res.status(200).send("Ping envoyé !");
 }
