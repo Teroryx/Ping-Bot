077a992b0d3a41012ca7d96fc9602b4e7d3043a2
Teroryx <enzoizinga23@gmail.com>
2025-11-10 19:36:42 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const fetch = (await import('node-fetch')).default;
-  const url = "https://ton-bot.onrender.com/"; // ton lien Render ici
+  const url = "https://https://s-one-bot.onrender.com/"; // ton lien Render ici
   await fetch(url);
   res.status(200).send("Ping envoyé !");
 }
